b077f4be7bc885c01e3d077f03947e9bcafce9d7
Melek Lassoued <lassouedm07@gmail.com>
2025-06-24 00:10:58 +0100

    feat: hide the slug field if the name is empty

--- schemaTypes/eventType.ts
+++ schemaTypes/eventType.ts
@@ -16,6 +16,7 @@ export const eventType = defineType({
         source: 'name',
       },
       validation: (rule) => rule.required().error('Required to generate a page on the page'),
+      hidden: ({document}) => !document?.name,
     }),
     defineField({
       name: 'eventType',
