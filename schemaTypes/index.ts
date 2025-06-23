7c06b1502f0391ac3c94155b1eff6af1fb5d08a8
Melek Lassoued <lassouedm07@gmail.com>
2025-06-24 00:02:20 +0100

    first commit

---
+++ schemaTypes/index.ts
@@ -0,0 +1,5 @@
+import {eventType} from './eventType'
+import {vennueType} from './venue'
+import {artistType} from './artistType'
+
+export const schemaTypes = [eventType, vennueType, artistType]
---
+++ schemaTypes/venue.ts
@@ -0,0 +1,13 @@
+import {defineField, defineType} from 'sanity'
+
+export const vennueType = defineType({
+  name: 'venue',
+  title: 'Venue',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'name',
+      type: 'string',
+    }),
+  ],
+})
