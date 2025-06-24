f698128d1939ac7da0b084602bde9a6b2dbedd54
Melek lassoued <lassouedm07@gmail.com>
2025-06-24 16:09:12 +0100

    feat: Enhance preview functionality in eventType schema with additional fields

--- schemaTypes/eventType.ts
+++ schemaTypes/eventType.ts
@@ -87,8 +87,30 @@ export const eventType = defineType({
   preview: {
     select: {
       title: 'name',
+      venue: 'venue.name',
       subtitle: 'headline.name',
       media: 'image',
+      date: 'date',
+      image: 'image',
+      artist: 'headline.name',
+    },
+    prepare({title, venue, artist, date, image}) {
+      const nameFormatted = title || 'Untitled event'
+      const dateFormatted = date
+        ? new Date(date).toLocaleDateString(undefined, {
+            month: 'short',
+            day: 'numeric',
+            year: 'numeric',
+            hour: 'numeric',
+            minute: 'numeric',
+          })
+        : 'No date'
+
+      return {
+        title: artist ? `${nameFormatted} (${artist})` : nameFormatted,
+        subtitle: venue ? `${dateFormatted} at ${venue}` : dateFormatted,
+        media: image || CalendarIcon,
+      }
     },
   },
 })
